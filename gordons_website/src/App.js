91615044a16f15bf6d3beb30b7af0560ded7d67d
CodeOfGordon <81336844+CodeOfGordon@users.noreply.github.com>
2023-09-09 16:55:20 -0400

    Completed caroussel

--- gordons_website/src/App.js
+++ gordons_website/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Logos_block from './components/Logos_block.js';
 import Caroussel_block from './components/Caroussel_block';
@@ -18,48 +19,59 @@ import python from './images/python.png';
 import travel from './images/travel_therapy.png';
 import pokemon from './images/pokemon_showdown.png';
 import wallpaper from './images/wallpaper_playlist.png';
-import left_arrow from './images/Left Arrow.png';
-import right_arrow from './images/Right Arrow.png';
-
 
 function App() {
+  const images_ = [travel, pokemon, wallpaper];
+  const headers = [
+    "Travel Therapy Discord Bot",
+    "Pokemon Showdown Demo",
+    "Wallpaper Engine Playlist Playlist"
+  ]
+  const texts = [
+    "We created a Discord bot using Python, the Co:here API, and Discord API. This bot uses AI to suggest places in Toronto to calm down depending on the input it receives.",
+    "We created a demo UI for a pokemon showdown game with randomized pokemon and movesets. This was created using Python and Tkinter.",
+    "I created this to help others create a playlist of their playlists! I achieved this using Wallpaper Engine’s commands, batch, vbs, and understanding task manager."
+  ];
+  
+  
+
   return (
-    <div className="App">
+    <div id="App">
       
       <span id="navbar-wrapper">
-        <ul className="navbar">
-          <a href="#navbar-wrapper" className="home">Home</a>
-          <a href="#about-me-wrapper" className="about-me">About me</a>
-          <a className="experience">Experience</a>
-          <a className="contact">Contact</a>
+        <ul id="navbar">
+          <a href="#navbar-wrapper">Home</a>
+          <a href="#about-me-wrapper">About me</a>
+          <a href="#skills-wrapper">Skills</a>
+          <a href="#experience-wrapper">Experience</a>
         </ul>
       </span>
       <body>
       <div id="home-introduction-wrapper">
-        <img className="author" alt="Author" src={author} />
-        <div className="info">
-          <h1 className="text-wrapper">Hi, I’m Gordon Huynh</h1>
-          <h1 className="subtext-wrapper">I am a student at the University of Waterloo with a passion for tech</h1>
-          <div className="Social-medias">
-            <a target="_blank" href="https://www.linkedin.com/in/gordon-huynh-02a5571b3/">
-              <img className="linkedin-symbole" alt="Linkedin symbole" src={linkedin} />
+        <img id="author" alt="Author" src={author} />
+        <div id="info">
+          <h1 id="text-wrapper">Hi, I’m Gordon Huynh</h1>
+          <h1 id="subtext-wrapper">I am a student at the University of Waterloo with a passion for learning</h1>
+          <div id="Social-medias">
+            <a target="noreferrer" href="https://www.linkedin.com/in/gordon-huynh-02a5571b3/">
+              <img id="linkedin-symbole" alt="Linkedin symbole" src={linkedin} />
             </a>
-            <a target="_blank" href="https://github.com/CodeOfGordon?tab=repositories">
-              <img className="github-invertocat" alt="Github invertocat" src={github} />
+            <a target="noreferrer" href="https://github.com/CodeOfGordon?tab=repositories">
+              <img id="github-invertocat" alt="Github invertocat" src={github} />
             </a>
-            <a className="resume-wrapper" target="_blank" href="https://drive.google.com/file/d/18oq8aBr6L-bAwDpONeU_5ohLOpXfCApU/view?usp=sharing">
-              <h1 className="resume">Resume</h1>
+            <a id="resume-wrapper" target="noreferrer" href="https://drive.google.com/file/d/18oq8aBr6L-bAwDpONeU_5ohLOpXfCApU/view?usp=sharing">
+              <h1 id="resume">Resume</h1>
             </a>
           </div>
         </div>
       </div>
       
-      <div className="city-wrapper">
-        <a className="More_info2" target="_blank" href="https://youindanang.com/dragon-bridge-da-nang-vietnam/">
+      <div id="city-wrapper">
+        <a id="More_info2" target="noreferrer" href="https://youindanang.com/dragon-bridge-da-nang-vietnam/">
           <img id="info2_icon" alt="Extra info" src={info_extra}/>
         </a>
-        <img className="city" alt="City" src={city} />
-        <img className="reflection" alt="Reflection of city" src={reflection} />
+        <img id="city" alt="City" src={city} />
+        <img id="reflection" alt="Reflection of city" src={reflection} />
       </div>
       
       <div id="about-me-wrapper" className="info_block">
@@ -71,8 +83,6 @@ function App() {
           <br/><br/>
           There are many things I have learned, yet to learn, and things that I will want 
           to learn in the future.
-          This is the main reason why I want to go into the field of tech, 
-          so I can keep life more interesting.
         </p>
       </div>
 
@@ -83,28 +93,20 @@ function App() {
             img1={git} img1alt={"git"} 
             img2={bash} img2alt={"bash"} 
             img3={C} img3alt={"C"} 
-            img4={javascript} img4alt={"javascript"} 
-            img5={react} img5alt={"react"} />
+            img4={javascript} img4alt={"javascript"} />
           <br/>
           <Logos_block 
-            img1={tailwind} img1alt={"tailwind"} 
-            img2={python} img2alt={"python"}/>
+            img1={react} img5alt={"react"}
+            img2={tailwind} img1alt={"tailwind"} 
+            img3={python} img2alt={"python"}/>
         </div>
       </div>
 
       <div id="experience-wrapper" className="info_block">
         <h1>Experience</h1>
-          <ul className="caroussel">
-            <button className="caroussel_button">
-              <img src={left_arrow} alt="left arrow of caroussel"/>
-            </button>
-            <Caroussel_block image={travel} altr={"travel therapy bot"}/>
-            <Caroussel_block/>
-            <Caroussel_block/>
-            <button className="caroussel_button">
-              <img src={right_arrow} alt="right arrow of caroussel"/>
-            </button>
-          </ul>
+        <div id="caroussel">
+          <Caroussel_block images={images_} altr={headers} titles={headers} paragraphs={texts}/>
+        </div>
       </div>
       </body>
     </div>
--- gordons_website/src/components/Caroussel_block.js
+++ gordons_website/src/components/Caroussel_block.js
@@ -1,7 +1,38 @@
-export default function Caroussel_block ({image, altr, txt}) {
-       return(
-           <span className="caroussel_slide">
-                <img src={image} alt={altr}/>
-            </span>
-       );
-   }
+import { useState } from "react";
+import left_arrow from '../images/Left Arrow.png'
+import right_arrow from '../images/Right Arrow.png';
+
+export default function Caroussel_block ({images, altr, titles, paragraphs}) {
+    const Carousel = ({images});
+    const Carousel_txt = ({paragraphs})
+    const [currIndex, setCurrIndex] = useState(0);
+    const slide_left = () => {
+        setCurrIndex((prevIndex) =>
+            prevIndex + 1 === images.length ? 0 : prevIndex + 1
+        );
+    };
+
+    const slide_right = () => {
+        setCurrIndex((prevIndex) =>
+            prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+    return(
+        <ul>
+            <button className="caroussel_button" id="left_arrow" onClick={slide_left}>
+                <img className="caroussel_button_img" src={left_arrow} alt="left arrow of caroussel"/>
+            </button>
+            <button className="caroussel_button" id="right_arrow" onClick={slide_right}>
+                <img src={right_arrow} alt="right arrow of caroussel"/>
+            </button>
+            <div id="caroussel_content">
+                <img key={currIndex} src={images[currIndex]} alt={altr}/>
+                <div id="caroussel_text">
+                    <h3>{titles[currIndex]}</h3>
+                    <p>{paragraphs[currIndex]}</p>
+                </div>
+            </div>
+            
+        </ul>
+    );
+}
