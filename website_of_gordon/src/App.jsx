2b5eb7a1ddaf3d12db2923e72d588e36b9847ca1
codeofgordon <gordonhuynh2@gmail.com>
2024-12-02 01:19:11 -0500

    Added new experience
    - Added powerbi dashboard

--- website_of_gordon/src/App.jsx
+++ website_of_gordon/src/App.jsx
@@ -4,13 +4,14 @@ import CarousselBlock from './components/CarousselBlock.jsx';
 
 function App() {
   const carrouselInfo = {
-    images: ['/caroussel/travel_therapy.png', '/caroussel/pokemon_showdown.png', '/caroussel/wallpaper_playlist.png', '/caroussel/express-test-message-board-sc.png'],
-    altr: ["Travel Therapy Discord Bot", "Pokemon Showdown Demo", "Wallpaper Engine Playlist Playlist", "Express Test Message Board"],
-    titles: ["Travel Therapy Discord Bot", "Pokemon Showdown Demo", "Wallpaper Engine Playlist Playlist", "Express Test Message Board"],
+    images: ['/caroussel/travel_therapy.png', '/caroussel/pokemon_showdown.png', '/caroussel/wallpaper_playlist.png', '/caroussel/express-test-message-board-sc.png', '/caroussel/spotify-dasboard.png'],
+    altr: ["Travel Therapy Discord Bot", "Pokemon Showdown Demo", "Wallpaper Engine Playlist Playlist", "Express Test Message Board", "PowerBI-Spotify-Dashboard"],
+    titles: ["Travel Therapy Discord Bot", "Pokemon Showdown Demo", "Wallpaper Engine Playlist Playlist", "Express Test Message Board", "PowerBI-Spotify-Dashboard"],
     links: ['https://github.com/alexszokolay/Uoft-ECKS',
       'https://github.com/CodeOfGordon/Pokemon-battle-simulator',
       'https://github.com/CodeOfGordon/Wallpaper-Engine-Playlist-Playlist',
-      'https://github.com/CodeOfGordon/test_express_message_board'
+      'https://github.com/CodeOfGordon/test_express_message_board',
+      'https://github.com/CodeOfGordon/PowerBI-Spotify-Dashboard'
     ]
   }
   return (
