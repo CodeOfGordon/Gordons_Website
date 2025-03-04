75deae05f15fe6d0fc7ac358ce5a6c39cc50be6a
codeofgordon <gordonhuynh2@gmail.com>
2025-03-03 20:17:01 -0500

    Updated experience and reordered caroussel exp

--- website_of_gordon/src/components/Experience/Experience.jsx
+++ website_of_gordon/src/components/Experience/Experience.jsx
@@ -2,14 +2,25 @@ import CarousselBlock from "./Caroussel/CarousselBlock";
 
 export default function Experience() {
     const carrouselInfo = {
-        images: ['/caroussel/travel_therapy.png', '/caroussel/pokemon_showdown.png', '/caroussel/wallpaper_playlist.png', '/caroussel/express-test-message-board-sc.png', '/caroussel/spotify-dasboard.png'],
-        altr: ["Travel Therapy Discord Bot", "Pokemon Showdown Demo", "Wallpaper Engine Playlist Playlist", "Express Test Message Board", "PowerBI-Spotify-Dashboard"],
-        titles: ["Travel Therapy Discord Bot", "Pokemon Showdown Demo", "Wallpaper Engine Playlist Playlist", "Express Test Message Board", "PowerBI-Spotify-Dashboard"],
+        images: ['/caroussel/travel_therapy.png', '/caroussel/spotify-dasboard.png', 
+            '/caroussel/azure_data_pipelining.png', '/caroussel/CxC_submission.png', 
+            '/caroussel/Chess Project.png', '/caroussel/wallpaper_playlist.png', 
+            '/caroussel/express-test-message-board-sc.png'],
+        altr: ['Travel Therapy Discord Bot', 'PowerBI-Spotify-Dashboard', 
+            'Azure Data Pipeline project', 'CxC Submission - Inflation Watch', 
+            'Group Chess Project',
+            "Wallpaper Engine Playlist Playlist", "Express Test Message Board"],
+        titles: ['Travel Therapy Discord Bot', 'PowerBI-Spotify-Dashboard', 
+            'Azure Data Pipeline project', 'CxC Submission - Inflation Watch', 
+            'Group Chess Project', 
+            'Wallpaper Engine Playlist Playlist', 'Express Test Message Board', ],
         links: ['https://github.com/alexszokolay/Uoft-ECKS',
-          'https://github.com/CodeOfGordon/Pokemon-battle-simulator',
+          'https://github.com/CodeOfGordon/PowerBI-Spotify-Dashboard',
+          'https://github.com/CodeOfGordon/Azure_Sales_Customer_Pipelining',
+          'https://github.com/CodeOfGordon/CxC-2025',
+          '',
           'https://github.com/CodeOfGordon/Wallpaper-Engine-Playlist-Playlist',
           'https://github.com/CodeOfGordon/test_express_message_board',
-          'https://github.com/CodeOfGordon/PowerBI-Spotify-Dashboard'
         ]
     }
     return(
