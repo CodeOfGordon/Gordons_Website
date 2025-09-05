def38b91b7b43325ac545eddd01da308584911d2
Code_Of_Gordon <gordonhuynh2@gmail.com>
2025-09-05 14:26:30 -0400

    Spelling adjustment

--- website_of_gordon/src/components/About-Me/AboutMe.jsx
+++ website_of_gordon/src/components/About-Me/AboutMe.jsx
@@ -5,7 +5,7 @@ export default function AboutMe() {
         <div id="about-me-wrapper" className="info-block">
             <h1>About me</h1>
             <p className='basic-text'>
-                I’m a student studying a Bachelors Degree in Math Economics, with an interest in data science and software development, 
+                I’m a student studying a Bachelors Degree in Math Economics with an interest in data science and software development 
                 and this is my portfolio. 
                 <br/><br/>
                 Feel free to take a look!
