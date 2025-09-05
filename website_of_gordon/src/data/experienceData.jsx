5332bb45157ef815313b4e489195953670128264
Code_Of_Gordon <gordonhuynh2@gmail.com>
2025-09-05 14:24:34 -0400

    Fixed links

--- website_of_gordon/src/data/experienceData.jsx
+++ website_of_gordon/src/data/experienceData.jsx
@@ -9,7 +9,7 @@ export const projects = [
     image: "/caroussel/spotify-dasboard.png",
     alt: "Spotify Dashboard",
     title: "Spotify Dashboard",
-    link: "https://github.com/CodeOfGordon/spotify-dashboard",
+    link: "https://github.com/CodeOfGordon/PowerBI-Spotify-Dashboard",
   },
   {
     image: "/caroussel/credit-card-fraud.png",
@@ -21,19 +21,19 @@ export const projects = [
     image: "/caroussel/Categorizing_spotify_songs.png",
     alt: "Categorizing Spotify Songs",
     title: "Categorizing Spotify Songs",
-    link: "https://github.com/CodeOfGordon/Categorizing_spotify_songs",
+    link: "https://github.com/CodeOfGordon/Categorizing-spotify-songs",
   },
   {
     image: "/caroussel/CxC_submission.png",
     alt: "CxC Submission on Times Series Forecasting",
     title: "CxC Submission on Times Series Forecasting",
-    link: "https://github.com/CodeOfGordon/cxc-submission",
+    link: "https://github.com/CodeOfGordon/CxC-2025",
   },
   {
     image: "/caroussel/azure_data_pipelining.png",
     alt: "Azure Data Pipelining",
     title: "Azure Data Pipelining",
-    link: "https://github.com/CodeOfGordon/azure-data-pipelining",
+    link: "https://github.com/CodeOfGordon/Azure_Sales_Customer_Pipelining",
   },
   {
     image: "/caroussel/Grow_helper.png",
@@ -45,12 +45,12 @@ export const projects = [
     image: "/caroussel/travel_therapy.png",
     alt: "Travel Therapy",
     title: "Travel Therapy",
-    link: "https://github.com/CodeOfGordon/travel-therapy",
+    link: "https://github.com/alexszokolay/Uoft-ECKS",
   },
   {
     image: "/caroussel/wallpaper_playlist.png",
     alt: "Wallpaper Playlist",
     title: "Wallpaper Playlist",
-    link: "https://github.com/CodeOfGordon/wallpaper-playlist",
+    link: "https://github.com/CodeOfGordon/Wallpaper-Engine-Playlist-Playlist",
   }
 ];
