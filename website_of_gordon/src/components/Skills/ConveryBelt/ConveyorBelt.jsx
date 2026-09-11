0e07c551926c82909e1a1abb6c03a6b945dae573
Code_Of_Gordon <gordonhuynh2@gmail.com>
2026-09-11 18:57:40 -0400

    Smooth skills conveyor

--- website_of_gordon/src/components/Skills/ConveryBelt/ConveyorBelt.jsx
+++ website_of_gordon/src/components/Skills/ConveryBelt/ConveyorBelt.jsx
@@ -1,45 +1,34 @@
-import * as SiIcons from "react-icons/si";
-import * as RiIcons from "react-icons/ri";
-import * as FaIcons from "react-icons/fa";
-import * as VscIcons from "react-icons/vsc";
-import * as LuIcons from "react-icons/lu";
-import * as HiIcons from "react-icons/hi2";
-import * as BsIcons from "react-icons/bs";
-import * as TbIcons from "react-icons/tb";
+import PropTypes from "prop-types";
 import "./ConveyorBelt.css";
 
-const iconPacks = {
-  'Si': SiIcons,
-  'Ri': RiIcons,
-  'Fa': FaIcons,
-  'Vs': VscIcons,
-  'Lu': LuIcons,
-  'Hi': HiIcons,
-  'Bs': BsIcons,
-  'Tb': TbIcons
-};
-
 export default function ConveyorBelt({ skills, direction = "belt-left", label }) {
-    return (
-        <div className={`conveyor-belt-wrapper ${direction}`}>
-            <p className="belt-label basic-text">{label}</p>
-            <div className={`conveyor-belt ${direction}`}>
-            {[...skills, ...skills].map((tech, idx) => {
-                // ^ Duplication trick so animation is seamless
-                // Get prefix (first part before uppercase letter)
-                // const match = tech.icon.match(/^([A-Za-z]+)[A-Z]/);
-                const prefix = tech.icon.substring(0, 2); 
-                const IconPack = iconPacks[prefix];
-                const Icon = IconPack ? IconPack[tech.icon] : null;
+  return (
+    <div className={`conveyor-belt-wrapper ${direction}`}>
+      <p className="belt-label basic-text">{label}</p>
+      <div className={`conveyor-belt ${direction}`}>
+        {[...skills, ...skills].map((tech, index) => {
+          const Icon = tech.icon;
+          const duplicate = index >= skills.length;
 
-                return (
-                    <div key={tech.name + idx} className="skill-icon">
-                        {Icon ? <Icon title={tech.name} /> : <span>{tech.name}</span>}
-                        <p>{tech.name}</p>
-                    </div>
-                );
-            })}
+          return (
+            <div key={`${tech.name}-${index}`} className="skill-icon" aria-hidden={duplicate ? "true" : undefined}>
+              <Icon title={tech.name} aria-hidden="true" />
+              <p>{tech.name}</p>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
+ConveyorBelt.propTypes = {
+  skills: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      icon: PropTypes.elementType.isRequired,
+    }),
+  ).isRequired,
+  direction: PropTypes.oneOf(["belt-left", "belt-right"]),
+  label: PropTypes.string.isRequired,
+};
--- website_of_gordon/src/components/Skills/Skills.jsx
+++ website_of_gordon/src/components/Skills/Skills.jsx
@@ -3,11 +3,11 @@ import ConveyorBelt from "./ConveryBelt/ConveyorBelt.jsx";
 
 export default function Skills() {
   return (
-    <div id="skills-wrapper" className="info-block">
-      <h1>Skills</h1>
+    <section id="skills-wrapper" className="info-block" aria-labelledby="skills-title">
+      <h1 id="skills-title">Skills</h1>
       <ConveyorBelt skills={languages} direction="belt-left" label="Languages" />
       <ConveyorBelt skills={frameworks_libraries} direction="belt-right" label="Frameworks/Libraries" />
       <ConveyorBelt skills={tools} direction="belt-left" label="Tools" />
-    </div>
+    </section>
   );
 }
